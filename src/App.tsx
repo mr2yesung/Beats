994cffc230ceea80d02a7ac2597027b189f1cfd7
mr2yesung <mryesung@naver.com>
2024-05-14 06:02:58 +0900

    scroll to top of page when location pathname changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/home/Home";
 import ProductDetail from "./pages/product-detail/ProductDetail";
 import NotFound from "./pages/not-found/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product">
-        <Route path=":productId" element={<ProductDetail />} />
-      </Route>
+    <>
+      <ScrollToTop />
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product">
+          <Route path=":productId" element={<ProductDetail />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
