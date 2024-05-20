7546a1e98b02691dc56afb9a678094749bd51d37
mr2yesung <mryesung@naver.com>
2024-05-21 01:15:44 +0900

    change products page display, show loading bar on the right portion only

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -12,21 +12,10 @@ function Products() {
   const { products, isLoading } = useProducts();
   const [currentPage, setCurrentPage] = useState(1);
 
-  if (isLoading) return <Loading />;
-
-  if (!products) return <Navigate to="/not-found" replace />;
-
-  const maxPage = Math.ceil(products.length / itemsPerPage);
-
   function resetCurrentPage() {
     setCurrentPage(1);
     window.scrollTo(0, 0);
   }
-
-  const pageDisplayProducts = products.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage,
-  );
 
   return (
     <div className="flex-1 bg-primary/5 py-8">
@@ -36,19 +25,36 @@ function Products() {
         </div>
 
         <div className="col-span-3">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            {pageDisplayProducts.map((product) => (
-              <Product key={product.id} {...product} />
-            ))}
+          {isLoading ? (
+            <Loading />
+          ) : products ? (
+            products.length > 0 ? (
+              <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                {products
+                  .slice(
+                    (currentPage - 1) * itemsPerPage,
+                    currentPage * itemsPerPage,
+                  )
+                  .map((product) => (
+                    <Product key={product.id} {...product} />
+                  ))}
 
-            <div className="col-span-3">
-              <PaginationComponent
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                maxPage={maxPage}
-              />
-            </div>
-          </div>
+                <div className="col-span-3">
+                  <PaginationComponent
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    maxPage={Math.ceil(products.length / itemsPerPage)}
+                  />
+                </div>
+              </div>
+            ) : (
+              <p className="flex h-full items-center justify-center text-lg">
+                No products found
+              </p>
+            )
+          ) : (
+            <Navigate to="/not-found" replace />
+          )}
         </div>
       </div>
     </div>
