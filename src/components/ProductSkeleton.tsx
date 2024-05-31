a0838e550c9d4188e0887480509d7bbfc8fdb360
mr2yesung <mryesung@naver.com>
2024-05-31 20:47:29 +0900

    fix product skeleton size difference with actual product card size

--- src/components/ProductSkeleton.tsx
+++ src/components/ProductSkeleton.tsx
@@ -4,16 +4,16 @@ import { Skeleton } from "./ui/skeleton";
 
 function ProductSkeleton() {
   return (
-    <Card className="overflow-hidden rounded-lg bg-white shadow-md">
+    <Card className="h-[517.5px] overflow-hidden rounded-lg bg-white shadow-md">
       <Skeleton className="aspect-square w-full object-cover" />
 
-      <CardContent className="p-4 underline-offset-2">
+      <CardContent className="p-4">
         <Skeleton className="mb-2 h-7 w-40 rounded-lg" />
-        <Skeleton className="h-6 w-full" />
+        <Skeleton className="h-[72px] w-full" />
       </CardContent>
 
       <CardFooter className="flex items-center justify-between px-4">
-        <Skeleton className="h-10 w-28 rounded-lg" />
+        <Skeleton className="h-7 w-[80px] rounded-lg" />
         <Button disabled>Add to Cart</Button>
       </CardFooter>
     </Card>
