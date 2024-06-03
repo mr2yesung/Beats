d9f33e1989080c549d6369cf87c97126059d1be8
mr2yesung <mryesung@naver.com>
2024-06-04 02:55:55 +0900

    invalidate order query for instant re-fetch after order creation success

--- src/hooks/useCreateOrder.ts
+++ src/hooks/useCreateOrder.ts
@@ -1,13 +1,15 @@
 import { useToast } from "@/components/ui/use-toast";
 import { supabaseCreateOrder, orderPostData } from "@/services/apiOrders";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 function useCreateOrder() {
+  const queryClient = useQueryClient();
   const { toast } = useToast();
 
   const { mutate: createOrder, isPending: isLoading } = useMutation({
     mutationFn: (orderData: orderPostData) => supabaseCreateOrder(orderData),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
       toast({
         description: "Order created successfully",
       });
