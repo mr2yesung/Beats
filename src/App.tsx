d649ee69515240c0b19b8db72d4495b3649bfe9e
mr2yesung <mryesung@naver.com>
2024-06-04 02:55:26 +0900

    lazy load components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
+import { Suspense, lazy } from "react";
 import { Outlet, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/home/Home";
-import Products from "./pages/products/Products";
-import ProductDetail from "./pages/product-detail/ProductDetail";
-import NotFound from "./pages/not-found/NotFound";
 import PageLayout from "./layout/PageLayout";
 import { CartProvider } from "./contexts/CartContext";
-import Policy from "./pages/policy/Policy";
-import About from "./pages/about/About";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/sign-up/SignUp";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "./components/ui/toaster";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Checkout from "./pages/checkout/Checkout";
-import Orders from "./pages/orders/Orders";
+import Loading from "./components/Loading";
+
+const Products = lazy(() => import("./pages/products/Products"));
+const ProductDetail = lazy(
+  () => import("./pages/product-detail/ProductDetail"),
+);
+const NotFound = lazy(() => import("./pages/not-found/NotFound"));
+const Policy = lazy(() => import("./pages/policy/Policy"));
+const About = lazy(() => import("./pages/about/About"));
+const Login = lazy(() => import("./pages/login/Login"));
+const SignUp = lazy(() => import("./pages/sign-up/SignUp"));
+const Checkout = lazy(() => import("./pages/checkout/Checkout"));
+const Orders = lazy(() => import("./pages/orders/Orders"));
 
 function App() {
   return (
@@ -23,33 +28,35 @@ function App() {
 
       <AuthProvider>
         <CartProvider>
-          <Routes>
-            <Route path="/" element={<PageLayout />}>
-              <Route index element={<Home />} />
-              <Route path="product">
-                <Route index element={<Products />} />
-                <Route path=":productId" element={<ProductDetail />} />
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<PageLayout />}>
+                <Route index element={<Home />} />
+                <Route path="product">
+                  <Route index element={<Products />} />
+                  <Route path=":productId" element={<ProductDetail />} />
+                </Route>
+                <Route path="about" element={<About />} />
+                <Route path="policy" element={<Policy />} />
+
+                <Route
+                  element={
+                    <ProtectedRoute>
+                      <Outlet />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route path="orders" element={<Orders />} />
+                  <Route path="checkout" element={<Checkout />} />
+                </Route>
               </Route>
-              <Route path="about" element={<About />} />
-              <Route path="policy" element={<Policy />} />
 
-              <Route
-                element={
-                  <ProtectedRoute>
-                    <Outlet />
-                  </ProtectedRoute>
-                }
-              >
-                <Route path="orders" element={<Orders />} />
-                <Route path="checkout" element={<Checkout />} />
-              </Route>
-            </Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </CartProvider>
       </AuthProvider>
 
